16cda658290d12d822194525820371ac6f89d7f5
neoMichael <40837668+neoMichael@users.noreply.github.com>
2020-06-30 15:00:27 +0100

    Department class created

---
+++ src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entity.Department;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Department o = new Department(1, "Customer Service");
+		
+		System.out.println(o);
+		
+
+	}
+
+}
